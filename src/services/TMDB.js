51d4ad9d5f9f123c4f9e46d6e52611ced9e3b936
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-08 11:44:06 +0530

    Update TMDB.js

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -42,7 +42,7 @@ export const tmdbApi = createApi({
 
     // Get Recommendations
     getRecommendations: builder.query({
-      query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+      query: ({ movieId, list }) => `/movie/${movieId}/${list}?api_key=${tmdbApiKey}`,
     }),
 
     // Get Actor
