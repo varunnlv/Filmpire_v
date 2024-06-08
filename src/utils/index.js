923172cbaaecb07dba22b023f75542ac4bc7f9de
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-08 13:45:49 +0530

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,7 +30,7 @@ export const createSessionId = async () => {
 
   if (token) {
     try {
-      const { data: { session_id } } = await moviesApi.post('authentication/session/new', {
+      const { data: { sessionId } } = await moviesApi.post('authentication/session/new', {
         request_token: token,
       });
       localStorage.setItem('sessionId', session_id);
