781cd862bee58d57d2eafd7614f667faeff4e463
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-08 17:55:20 +0530

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import { Grid } from '@mui/material';
 import useStyles from './styles';
 
 
-import {Movie} from '../index';
+import {Movie} from '../Movie/Movie';
 
 
 function MovieList({ movies, numberOfMovies, excludeFirst }) {
