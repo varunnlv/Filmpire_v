e8ef170750c54ed8834759d784e77ae341773c63
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-08 15:36:43 +0530

    Update Actors.jsx

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -6,8 +6,8 @@ import { ArrowBack } from '@mui/icons-material';
 import useStyles from './styles';
 import { useGetActorQuery, useGetMoviesByActorIdQuery } from '../../services/TMDB';
 
-import {MovieList} from '../MovieList';
-import {Pagination} from '../Pagination';
+import {MovieList} from '../MovieList/MovieList';
+import {Pagination} from '../Pagination/Pagination';
 
 
 function Actors() {
