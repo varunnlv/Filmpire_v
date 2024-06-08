0944fb9f5489cf7e7c36532b45dd983285e54ea9
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-08 16:05:42 +0530

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 
 import { useGetListQuery } from '../../services/TMDB';
 
-import {RatedCards} from '../RatedCards/RatedCards';
+import {RatedCards} from '../index';
 
 function Profile() {
   const { user } = useSelector((state) => state.user);
